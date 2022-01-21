3653e9415f109a8d5a6010c08f08d10efc63805a
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-01-21 14:06:35 -0300

    voltou ao normal

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CacheName = 'v2';
-/*const resourcesToPrecache = ['./index.html','./css/style.css','./manifest.json','./192x192-icon.png','./512x512-icon.png','./maskable_icon.png','./img-conf/nuvem.png','./img-conf/coffee.png','./img-conf/net.png'];*/
+const resourcesToPrecache = ['./index.html','./css/style.css','./manifest.json','./192x192-icon.png','./512x512-icon.png','./maskable_icon.png','./img-conf/nuvem.png','./img-conf/coffee.png','./img-conf/net.png'];
 
-const resourcesToPrecache = ['https://wesley3king.github.io/link/index.html','https://wesley3king.github.io/link/css/style.css','https://wesley3king.github.io/link/manifest.json','.https://wesley3king.github.io/link/192x192-icon.png','https://wesley3king.github.io/link/512x512-icon.png','https://wesley3king.github.io/link/maskable_icon.png','https://wesley3king.github.io/link/img-conf/nuvem.png','https://wesley3king.github.io/link/img-conf/coffee.png','https://wesley3king.github.io/link/img-conf/net.png'];
+/*const resourcesToPrecache = ['https://wesley3king.github.io/link/index.html','https://wesley3king.github.io/link/css/style.css','https://wesley3king.github.io/link/manifest.json','.https://wesley3king.github.io/link/192x192-icon.png','https://wesley3king.github.io/link/512x512-icon.png','https://wesley3king.github.io/link/maskable_icon.png','https://wesley3king.github.io/link/img-conf/nuvem.png','https://wesley3king.github.io/link/img-conf/coffee.png','https://wesley3king.github.io/link/img-conf/net.png'];*/
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CacheName)
