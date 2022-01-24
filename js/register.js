4933cc27605f14153266dff3fe84c43c14127a2b
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-01-24 10:20:12 -0300

    go

--- js/register.js
+++ js/register.js
@@ -1,6 +1,6 @@
 //prompt de instalação
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('./sw.js', {scope: './'})
     .then((reg) => {
       console.log(`serviceWorker registrado ${serviceWorker}`);
     }).catch((e) => {
--- sw.js
+++ sw.js
@@ -1,12 +1,31 @@
 const CacheName = 'v3';
-const resourcesToPrecache = ['./index.html','./css/style.css','./manifest.json','./192x192-icon.png','./512x512-icon.png','./maskable_icon.png','./img-conf/nuvem.png','./img-conf/coffee.png','./img-conf/net.png'];
+const resourcesToPrecache = [
+  '/',
+  './index.html',
+  './css/style.css',
+  './manifest.json',
+  './192x192-icon.png',
+  './512x512-icon.png',
+  './maskable_icon.png',
+  './img-conf/nuvem.png',
+  './img-conf/coffee.png',
+  './img-conf/net.png'
+];
 
-/*const resourcesToPrecache = ['https://wesley3king.github.io/link/index.html','https://wesley3king.github.io/link/css/style.css','https://wesley3king.github.io/link/manifest.json','.https://wesley3king.github.io/link/192x192-icon.png','https://wesley3king.github.io/link/512x512-icon.png','https://wesley3king.github.io/link/maskable_icon.png','https://wesley3king.github.io/link/img-conf/nuvem.png','https://wesley3king.github.io/link/img-conf/coffee.png','https://wesley3king.github.io/link/img-conf/net.png'];*/
+/*const resourcesToPrecache = ['https://wesley3king.github.io/link/index.html','https://wesley3king.github.io/link/css/style.css','https://wesley3king.github.io/link/manifest.json','.https://wesley3king.github.io/link/192x192-icon.png','https://wesley3king.github.io/link/512x512-icon.png','https://wesley3king.github.io/link/maskable_icon.png','https://wesley3king.github.io/link/img-conf/nuvem.png','https://wesley3king.github.io/link/img-conf/coffee.png','https://wesley3king.github.io/link/img-conf/net.png'];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CacheName)
     .then(cache => (cache.addAll(resourcesToPrecache))),
     )
+});*/
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CacheName).then(function (cache) {
+      return cache.addAll(resourcesToPrecache);
+    })
+  )
 });
 // Clear cache on activate
 
