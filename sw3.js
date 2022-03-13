40c7bebb15dc5a9405698415a9bb1a0fc206bc32
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-03-13 18:36:53 -0300

    update

--- sw3.js
+++ sw3.js
@@ -1,98 +1,57 @@
-var filesToAdd = ['./',
-    './index.html',
-    './css/style.css',
-    './js/index.js'
-]
+// Criando um nome para o arquivo de cache
+const staticCache = "version1";
 
-self.addEventListener('install', function(event) {
+// Lista de arquivos que devem ser cacheados
+const files = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/index.js'/*,
+  './assets/styles/main.min.css',
+  './assets/scripts/main.min.js',*/
+];
+
+// Faz cache dos arquivos ao instalar
+this.addEventListener("install", event => {
+  this.skipWaiting();
+
   event.waitUntil(
-    caches.open('gefy-pwa')
-      .then(function(cache) {
-        return cache.add('./index.html');
-      })
-  );
+    caches.open(staticCache)
+      .then(cache => {
+        return cache.addAll(files);
+    })
+  )
 });
 
-//activate
+// Limpa o cache antigo
 this.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames
-          .filter(cacheName => (cacheName.startsWith('gefy-')))
-          .filter(cacheName => (cacheName !== CACHE_NAME))
+          .filter(cacheName => (cacheName.startsWith('meu-site-')))
+          .filter(cacheName => (cacheName !== staticCache))
           .map(cacheName => caches.delete(cacheName))
       );
     })
   );
 });
-        //fetch
-        // adicinamos o listener do evento fetch
-self.addEventListener('fetch', function(event) {
-  // sempre que esse evento for disparado, responda da seguinte forma:
+
+// Responde o request direto do cache
+this.addEventListener("fetch", event => {
   event.respondWith(
-    // verique se a nossa request existe e está online
-    checkResponse(event.request)
-      .catch(function() {
-        // se não for encontrada procure no nosso cache se ela existe
-        return returnFromCache(event.request)}
-      ));
-  // aguarde a resposta, quando ela existir adicione ao nosso cache
-  event.waitUntil(addToCache(event.request));
-});
-
-// verifique se o response existe
-var checkResponse = function(request) {
-  return new Promise(function(fulfill, reject) {
-    // tente resolver a promise fazendo o fetch (online)
-    fetch(request)
-      .then(function(response) {
-        // verifica se o status retornado é diferente de 404
-        if(response.status !== 404) {
-          // se não foi 404, então achou algo!
-          fulfill(response)
-        } else {
-          // não encontrou nada
-          reject()
+    caches.match(event.request)
+      .then(response => {
+        // Retorna o cache
+        if (response) {
+          return response;
         }
-      // fetch foi rejeitado, possívelmente está offline
-      }, reject)
-  });
-};
-
-// se o fetch foi rejeitado, vamos ao cache
-var returnFromCache = function(request){
-  // abra o cacha 'gefy-pwa'
-  return caches.open('gefy-pwa')
-    // a abertura retorna uma promise com o cache
-    .then(function (cache) {
-      // verifica se a request existe no nosso cache
-      return cache.match(request)
-        .then(function (matching) {
-          // não encontrou nada
-          if(!matching || matching.status == 404) {
-            // redireciona pra página de offline
-            // pode ser qualquer página que não precise de conexão.
-            return cache.match('/404.html')
-          } else {
-            // retorna a request com o resultado do cache.
-            return matching
-          }
-        });
-    });
-};
-
-// tudo feito, vamos atualizar o cache
-var addToCache = function(request) {
-  // abra o cacha 'gefy-pwa'
-  return caches.open('gefy-pwa')
-    // a abertura retorna uma promise com o cache
-    .then(function (cache) {
-      // já sabemos que ele encontrou, entrou faz o fetch
-      return fetch(request)
-        .then(function (response) {
-          // pega o retorno dessa request e adiciona no nosso cache
-          return cache.put(request, response);
-        });
-    });
-};
+        // Faz a requisição
+        return fetch(event.request);
+      })
+      .catch(() => {
+        // Mostra uma página de offline
+        return caches.match('./index.html');
+      })
+  )
+});
